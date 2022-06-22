954ce0d1f2cbc6c77d229a90ba29a62fe7f5dfe0
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-06-22 16:16:30 +0200

    Updated ApiCall migration + Scaffolded Favorite

--- db/migrate/20220622125025_create_api_calls.rb
+++ db/migrate/20220622125025_create_api_calls.rb
@@ -1,7 +1,7 @@
 class CreateApiCalls < ActiveRecord::Migration[7.0]
   def change
     create_table :api_calls do |t|
-      t.string :api_key
+      t.string :api_key, null: false
       t.references :game, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
