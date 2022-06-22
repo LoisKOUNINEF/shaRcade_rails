c65c47ada5aa9498e391ac482c0b2294add6ec94
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-22 19:51:35 +0200

    sendgrid credentials & envionments config

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
 require_relative "application"
 
+ActionMailer::Base.smtp_settings = {
+  :user_name => Rails.application.credentials.sendgrid.username,
+  :password => Rails.application.credentials.sendgrid.password,
+  :domain => 'sharcade-dev.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # Initialize the Rails application.
 Rails.application.initialize!
