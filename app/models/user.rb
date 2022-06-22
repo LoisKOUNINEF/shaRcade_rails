d286c68031e90f0e0d7b076251fa09f6f6b3d344
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-22 19:52:05 +0200

    welwome email after create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,35 @@
 class User < ApplicationRecord
+ after_create :welcome_send
 
-  devise :database_authenticatable, :registerable,
-  :jwt_authenticatable,
-  jwt_revocation_strategy: JwtDenylist
+ devise :database_authenticatable, :registerable,
+ :jwt_authenticatable,
+ jwt_revocation_strategy: JwtDenylist
 
-  validates :nickname, presence: true, length: { in: 3..12 }
-  validates :firstname, length: { in: 3..12 }
-  validates :lastname, length: { in: 3..12 }
-  validates :firstname, length: { in: 3..12 }
+ validates :nickname, presence: true, length: { in: 3..12 }
+ validates :firstname, length: { in: 3..12 }
+ validates :lastname, length: { in: 3..12 }
+ validates :firstname, length: { in: 3..12 }
 
-  enum role: [:player, :editor, :admin]
-  after_initialize :set_default_role, :if => :new_record?
+ enum role: [:player, :editor, :admin]
+ after_initialize :set_default_role, :if => :new_record?
 
-  has_many :scores
-  has_many :api_calls
-  has_many :favorites
-  has_many :feedbacks
+ has_many :scores
+ has_many :api_calls
+ has_many :favorites
+ has_many :feedbacks
 
-  def what_role?
-    return self.role
-  end
+ def what_role?
+  return self.role
+end
 
-  private
 
-  def set_default_role
-    return self.role ||= :player
-  end
+def welcome_send
+  UserMailer.welcome_email(self).deliver_now
+end
+
+private
+
+def set_default_role
+  return self.role ||= :player
+end
 end
