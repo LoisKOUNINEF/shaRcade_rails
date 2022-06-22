bfd8224d11703f0add7b2d8ed5455e54affe3346
Loïs Kouninef <97851048+LoisKOUNINEF@users.noreply.github.com>
2022-06-22 19:44:37 +0200

    Update game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,7 +5,7 @@ class Game < ApplicationRecord
   has_many :feedbacks
   has_one :api_call
   has_many :scorers, through: :scores, class_name: :user          # Game player reached via related hi-score(s), hence the "scorer" alias
-  has_many :fans, through: favorites, class_name: :user           # Game player reached via his/her favorites' list, hence the "fan" alias
-  has_many :evaluators, through: feedbacks, class_name: :user     # Game player or editor reached via existing feedbacks, hence the "evaluator" alias
+  has_many :fans, through: :favorites, class_name: :user           # Game player reached via his/her favorites' list, hence the "fan" alias
+  has_many :evaluators, through: :feedbacks, class_name: :user     # Game player or editor reached via existing feedbacks, hence the "evaluator" alias
   has_one :owner, through: :api_calls, class_name: :user          # Game editor, hence the "owner" alias
 end
