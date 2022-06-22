28f2fc7fedec2e7247db25b5af4e1cc59b0743a0
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-22 17:59:05 +0200

    add enum role to user, default to :player

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,21 @@ class User < ApplicationRecord
   validates :lastname, length: { in: 3..12 }
   validates :firstname, length: { in: 3..12 }
 
+  enum role: [:player, :editor, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
   has_many :scores
   has_many :api_calls
   has_many :favorites
   has_many :feedbacks
+
+  def what_role?
+    return self.role
+  end
+
+  private
+
+  def set_default_role
+    return self.role ||= :player
+  end
 end
