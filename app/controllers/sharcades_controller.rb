cbeb06b728f0363a751d1bf15db4595d703dc091
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-07-01 20:16:59 +0200

    Managed (at last) to correctly send and retreat a 2-level-deep hash standing for the message a given game should be sending us so as to store its hi-score

--- app/controllers/sharcades_controller.rb
+++ app/controllers/sharcades_controller.rb
@@ -11,8 +11,8 @@ class SharcadesController < ApplicationController
     #   "user_email" : "player_01@yopmail.com"}
     # }
 
-    # REMINDER - Curl command line for test
-    # curl -X POST -d "{hiscore:9999,api_key:'fds4554FDS45fdsf',user_email:'player_01@yopmail.com'}" -H "Content-Type: application/json" http://localhost:3000/sharcade_api
+    # REMINDER - Curl command line for tests
+    # curl -X POST -H 'Content-Type: application/json' -d '{"user_email":"player_01@yopmail.com","api_key":"D4b92SXc0j1SdxW","hiscore":999999}' 'http://localhost:3000/sharcade_api'
 
     # if ()
     #   render json: {
@@ -26,28 +26,38 @@ class SharcadesController < ApplicationController
     #   }
     # end
 
-    my_received_params = sharcade_params
-    puts "***"
-    puts my_received_params
-    puts "***"
-
-    keygen_1 = ApiCallsController.shaRcadekeygen(16)
-    keygen_2 = ApiCallsController.shaRcadekeygen(16)
-    @my_api_response_1 = {id: 666, api_key: keygen_1, game_id: 1, user_id: 1}
-    @my_api_response_2 = {user_id: 7, game_id: 1, user_key: keygen_2, user_id: 1}
+    # puts "***"
+    # puts my_received_params
+    # puts "***"
+    my_filtered_params = sharcade_params
+    # puts "*** Hi-Score: " + my_filtered_params.dig(:hiscore)
+    # puts "*** API Key: " + my_filtered_params.dig(:api_key)
+    # puts "*** user_email: " + my_filtered_params.dig(:user_email)
+    puts "****"
+    puts "  > user_email"
+    puts my_filtered_params[:user_email]
+    puts "  > hiscore"
+    puts my_filtered_params[:hiscore].to_s + "toto"
+    puts "  > api_key:"
+    puts my_filtered_params[:api_key]
+    puts "****"
+    # keygen_1 = ApiCallsController.shaRcadekeygen(16)
+    # keygen_2 = ApiCallsController.shaRcadekeygen(16)
+    # @my_api_response_1 = {id: 666, api_key: keygen_1, game_id: 1, user_id: 1}
+    # @my_api_response_2 = {user_id: 7, game_id: 1, user_key: keygen_2, user_id: 1}
     
     # response = { :message =>,
     #              :object1 => @my_api_response_1, 
     #              :object2 => @my_api_response_2 }
     # render :json => response
     
-    puts "*** Received request on ShaRcade API entry point"
-    render json: {
-      message: "Got your request and here is the result, Dude!",
-      game: @my_api_response_1,
-      apicall: @my_api_response_2
-    }, status: :ok
-    puts "*** Treated request. Goodbye!"
+    # puts "*** Received request on ShaRcade API entry point"
+    # render json: {
+    #   message: "Got your request and here is the result, Dude!",
+    #   game: @my_api_response_1,
+    #   apicall: @my_api_response_2
+    # }, status: :ok
+    # puts "*** Treated request. Goodbye!"
 
   end
 
@@ -60,7 +70,10 @@ class SharcadesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def sharcade_params
-      params.permit(:hiscore, :api_key, :user_email)
+      # params.permit(:hiscore, :api_key, :user_email)
+      #params.permit(:score_token, :hiscore, :api_key, :user_email)
+      # params.require(:sharcade).permit(:hiscore, :api_key, :user_email)
+      params.require(:score_token).permit(:hiscore, :api_key, :user_email)
     end
 
 end
