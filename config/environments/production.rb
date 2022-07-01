57c1ba470a5d34f88b78bb51e247762a4a98bfd9
LoisKOUNINEF <lkouninef@gmail.com>
2022-07-01 15:11:06 +0200

    change mailer host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,5 +87,5 @@ Rails.application.configure do
   # shaRcade mailer
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host =>
-    'sharcade-dev.herokuapp.com' }
+    'sharcade.herokuapp.com' }
   end
